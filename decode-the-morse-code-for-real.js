8e8fa2c89d3dfb1621be2041c478731a4f775c25
Xianming Zhong <chinesedfan@qq.com>
2017-06-17 21:32:26 +0800

    k-means, 2 failed

--- decode-the-morse-code-for-real.js
+++ decode-the-morse-code-for-real.js
@@ -1,30 +1,46 @@
-var calculateOffset = function(counts, rate) {
-    return counts.reduce((sum, c) => {
-        if (c < rate * (1 + 3) / 2) sum += Math.abs(c - rate);
-        else if (c < rate * (3 + 7) / 2) sum += Math.abs(c - rate * 3);
-        else sum += Math.abs(c - rate * 7);
-        return sum;
+var divideIntoGroups = function(nums, centers) {
+    var gs = centers.map((c) => {
+        return {};
+    });
+    var sum = nums.reduce((sum, n) => {
+        var delta;
+        centers.some((c, i) => {
+            if (i == centers.length - 1 || n < (centers[i] + centers[i + 1]) / 2) {
+                delta = Math.abs(n - c);
+                gs[i][n] = 1;
+                return true;
+            }
+        });
+        return sum + delta * delta;
     }, 0);
+    return {gs: gs, sum: sum};
+};
+var getNewCenter = function(nums) {
+    if (!nums.length) return -1;
+    return nums.map((s) => parseInt(s)).reduce((sum, n) => sum + n, 0) / nums.length;
 };
 
-var determinateRate = function(counts, min, max, minOffset, maxOffset) {
-    while (Math.abs(max - min) > 0.0001) {
-        var mid = (min + max) / 2;
-        var midOffset = calculateOffset(counts, mid);
-        if (minOffset < maxOffset) {
-            max = mid;
-            maxOffset = midOffset;
-        } else {
-            min = mid;
-            minOffset = midOffset;
-        }
+var kmeans = function(nums, centers) {
+    var oldState = divideIntoGroups(nums, centers);
+
+    while (1) {
+        centers = oldState.gs.map((group) => getNewCenter(Object.keys(group)))
+                .map((c, i, list) => {
+                    // fix empty groups have no center
+                    return c < 0 ? (list[i - 1] + list[i + 1]) / 2 : c;
+                });
+
+        var newState = divideIntoGroups(nums, centers);
+        if (Math.abs(oldState.sum - newState.sum) < 0.01) break;
+        oldState = newState;
     }
-    return min;
+    return newState.gs;
 };
 
 var decodeBitsAdvanced = function(bits){
     // trim 0s
     bits = bits.replace(/^0+/, '').replace(/0+$/, '');
+
     // count 0s and 1s
     var map0 = {}, map1 = {};
     bits.replace(/0+|1+/g, (match) => {
@@ -32,38 +48,35 @@ var decodeBitsAdvanced = function(bits){
         map[match.length] = 1;
         return match;
     });
-    // dividing rates into groups(not consider only g3 and g7)
     var count0 = Object.keys(map0).sort((a, b) => a - b).map((k) => parseInt(k));
     var count1 = Object.keys(map1).sort((a, b) => a - b).map((k) => parseInt(k));
-    // console.log(JSON.stringify(count0));
-    // console.log(JSON.stringify(count1));
+
+    // determinate how many centers(ignore only 3 & 7)
     var min = count0.length ? Math.min(count0[0], count1[0]) : count1[0];
     var max = count0.length
             ? Math.max(count0[count0.length - 1], count1[count1.length - 1])
             : count1[count1.length - 1];
-    if (max >= min * (3 + 7) / 2) max /= 7;
-    else if (max >= min * (1 + 3) / 2) max /= 3;
+    var centers;
+    if (max >= min * (3 + 7) / 2) {
+        min = max / 7;
+        centers = [min, min * 3, min * 7];
+    } else if (max >= min * (1 + 3) / 2) {
+        min = max / 3;
+        centers = [min, min * 3];
+    } else {
+        centers = [min];
+    }
 
+    // k-means
     var counts = Object.keys(Object.assign({}, map0, map1)).map((k) => parseInt(k));
-    var rate = determinateRate(counts, min, max, calculateOffset(counts, min), calculateOffset(counts, max));
-    // console.log(min, max, rate);
-
-    var g1 = {}, g3 = {}, g7 = {};
-    count0.concat(count1).forEach((c) => {
-        if (c < rate * (1 + 3) / 2) g1[c] = 1;
-        else if (c < rate * (3 + 7) / 2) g3[c] = 1;
-        else g7[c] = 1;
-    });
-    // console.log(JSON.stringify(g1));
-    // console.log(JSON.stringify(g3));
-    // console.log(JSON.stringify(g7));
+    var gs = kmeans(counts, centers);
 
     return bits.replace(/0+|1+/g, (match) => {
         if (match[0] == '1') {
-            return g1[match.length] ? '.' : '-';
+            return gs[0][match.length] ? '.' : '-';
         } else {
-            if (g1[match.length]) return '';
-            if (g3[match.length]) return ' ';
+            if (gs[0][match.length]) return '';
+            if (gs[1][match.length]) return ' ';
             return '   ';
         }
     });
