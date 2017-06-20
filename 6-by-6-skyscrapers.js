9680fe0d4144f7c0d1a09302dc3fa30ae48ae92c
Xianming Zhong <chinesedfan@qq.com>
2017-06-20 14:56:48 +0800

    optimized to half time by dividing into groups

--- 6-by-6-skyscrapers.js
+++ 6-by-6-skyscrapers.js
@@ -1,11 +1,12 @@
 function solvePuzzle(clues) {
     const n = clues.length / 4;
     const ps = generatePermutations(n);
+    const seenMap = dividedIntoGroups(ps);
 
     const indexes = new Array(n).fill(-1); // index of each row in permutations
     let row = 0;
     while (1) {
-        if (findIndexForRow(clues, ps, indexes, row)) {
+        if (findIndexForRow(clues, ps, seenMap, indexes, row)) {
             row++;
             // solved
             if (row >= n) break;
@@ -34,16 +35,48 @@ function generatePermutations(n) {
     }
     return ps;
 }
+function dividedIntoGroups(ps) {
+    // seen -> index
+    const leftMap = {}, rightMap = {}, totalMap = {};
+    for (let i = 0; i < ps.length; i++) {
+        const left = seenFromLeft(ps[i]);
+        if (!leftMap[left]) leftMap[left] = [];
+        leftMap[left].push(i);
 
-function findIndexForRow(clues, ps, indexes, row) {
-    while (++indexes[row] < ps.length) {
-        const heights = ps[indexes[row]];
-        // check from left and right
-        const left = clues[clues.length - 1 - row];
-        if (left && left != seenFromLeft(heights)) continue;
-        const right = clues[clues.length / 4 + row];
-        if (right && right != seenFromRight(heights)) continue;
+        const right = seenFromRight(ps[i]);
+        if (!rightMap[right]) rightMap[right] = [];
+        rightMap[right].push(i);
 
+        if (!totalMap[left]) totalMap[left] = {};
+        if (!totalMap[left][right]) totalMap[left][right] = [];
+        totalMap[left][right].push(i);
+    }
+    return {
+        left: leftMap,
+        right: rightMap,
+        total: totalMap
+    };
+}
+
+function findIndexForRow(clues, ps, seenMap, indexes, row) {
+    const left = clues[clues.length - 1 - row];
+    const right = clues[clues.length / 4 + row];
+    // candidate indexes
+    let candidates;
+    if (left && right) {
+        candidates = seenMap.total[left][right];
+    } else if (left) {
+        candidates = seenMap.left[left];
+    } else if (right) {
+        candidates = seenMap.right[right];
+    } else {
+        candidates = new Array(ps.length - 1 - indexes[row]).fill(0).map((x, i) => indexes[row] + 1 + i);
+    }
+
+    for (let ci = 0; ci < candidates.length; ci++) {
+        if (indexes[row] >= candidates[ci]) continue;
+
+        const heights = ps[candidates[ci]];
         // check columns
         const hasError = heights.some((h, i) => {
             const arr = [];
@@ -66,6 +99,7 @@ function findIndexForRow(clues, ps, indexes, row) {
         });
         if (hasError) continue;
 
+        indexes[row] = candidates[ci];
         return true;
     }
     return false;
