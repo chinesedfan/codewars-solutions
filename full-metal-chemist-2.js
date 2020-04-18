427296fc8f9bd7eefcf7bd5105875baab5f2057b
Xianming Zhong <chinesedfan@qq.com>
2020-04-19 00:29:00 +0800

    add template

---
+++ full-metal-chemist-2.js
@@ -0,0 +1,12 @@
+// Number:        1      2      3...
+const RADICALS    = ['meth', 'eth', 'prop', 'but',   'pent',  'hex',  'hept',  'oct',  'non',  'dec',  'undec',  'dodec',  'tridec',  'tetradec',  'pentadec',  'hexadec',  'heptadec',  'octadec',  'nonadec']
+const MULTIPLIERS = [        'di',  'tri',  'tetra', 'penta', 'hexa', 'hepta', 'octa', 'nona', 'deca', 'undeca', 'dodeca', 'trideca', 'tetradeca', 'pentadeca', 'hexadeca', 'heptadeca', 'octadeca', 'nonadeca']
+
+const SUFFIXES    = [         'ol',      'al', 'one', 'oic acid', 'carboxylic acid',                'oate',               'ether', 'amide', 'amine', 'imine', 'benzene', 'thiol',    'phosphine', 'arsine']
+const PREFIXES    = ['cyclo', 'hydroxy',       'oxo',             'carboxy',         'oxycarbonyl', 'anoyloxy', 'formyl', 'oxy',   'amido', 'amino', 'imino', 'phenyl',  'mercapto', 'phosphino', 'arsino', 'fluoro', 'chloro', 'bromo', 'iodo']
+
+// Note that alkanes, alkenes alkynes, and akyles aren't present in these lists
+
+function parse(name) {
+    // Parse the name given as argument in the constructor and output the dict representing the raw formula
+}
