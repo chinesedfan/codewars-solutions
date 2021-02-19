c9908733c6e0deba06298913825db9d899f809d7
Xianming Zhong <chinesedfan@qq.com>
2021-02-19 22:19:45 +0800

    solve

--- pointer-monster.c
+++ pointer-monster.c
@@ -1,29 +1,29 @@
 #include <stdio.h>
 
 char a[4][7] = {"Common", "Point", "Boost", "Better"};
-/* Insert type here */ b[4] = {a+3, a+1, a, a+2};
+/* Insert type here */char (*b[4])[7] = {a+3, a+1, a, a+2};
 
-/* Insert type here */ c(void)
+/* Insert type here */char (*(*c(void))[4])[7]
 {
     return &b;
 }
 
-/* Insert type here */ d(void)
+/* Insert type here */char (**d(void))[7]
 {
     return c()[1] - 3;
 }
 
 char buf[256];
 
-char *pointer_monster(/* Insert type here */ f)
+char *pointer_monster(/* Insert type here */char (**f(void))[7])
 {
     int len;
     
-    len  = sprintf(buf, "%s", *f()[/* ? */]);
-    len += sprintf(buf + len, "%s ", *((**f)()-1)[0]+/* ? */);
-    len += sprintf(buf + len, "%s", (*f())[/* ? */]-4);
-    len += sprintf(buf + len, "%s", f()[/* ? */][2]+3);
-    len += sprintf(buf + len, "%s", *((**f)()-1)[0]+/* ? */);
+    len  = sprintf(buf, "%s", *f()[/* ? */0]);
+    len += sprintf(buf + len, "%s ", *((**f)()-1)[0]+/* ? */4);
+    len += sprintf(buf + len, "%s", (*f())[/* ? */0]-4);
+    len += sprintf(buf + len, "%s", f()[/* ? */1][2]+3);
+    len += sprintf(buf + len, "%s", *((**f)()-1)[0]+/* ? */4);
     return buf;
 }
 
@@ -33,15 +33,13 @@ char *pointer_monster(/* Insert type here */ f)
 
 // When ready, uncomment the following lines to test your code
 
-//extern /* Insert type here */ pointer_monster;
-//extern /* Insert type here */ d;
+extern /* Insert type here */ char *pointer_monster(char (**f(void))[7]);
+extern /* Insert type here */ char (**d(void))[7];
 
 Test(Pointer_Monster, Basic_Test)
 {
-    /*
     char *result = pointer_monster(d);
     cr_assert_str_eq(result, "Pointer monster",
         "Failed to produce \"Pointer monster\"; result was: %s", result);
-    */
     cr_assert(1);
 }
