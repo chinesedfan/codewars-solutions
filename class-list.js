6af89386a90995753e3cdd5950a5bc2bf59a5f8c
Xianming Zhong <chinesedfan@qq.com>
2021-02-24 20:36:11 +0800

    refactor with generator

--- class-list.js
+++ class-list.js
@@ -15,11 +15,18 @@ class List {
     })
   }
   static cycle(xs) {
+    if (xs.infinite) return new List(xs)
+
     return new List(function* () {
-      let i = 0
+      let g = xs.createGenerator()
       while (1) {
-        yield xs.get(i % xs.length())
-        i++
+        let obj = g.next()
+        if (obj.done) {
+          g = xs.createGenerator()
+          obj = g.next()
+          if (obj.done) throw new Error('no `cycle` for empty list')
+        }
+        yield obj.value
       }
     })
   }
@@ -30,11 +37,25 @@ class List {
     return new List(xs)
   }
 
-  constructor(list) {
+  createGenerator() {
+    const list = this.list
+    const gf = this.gf || function* () {
+      yield* list
+    }
+    return gf()
+  }
+
+  constructor(list, infinite = true) {
     if (Array.isArray(list)) {
       this.list = list
-    } else {
-      this.g = list()
+      this.infinite = false
+    } else if (list instanceof List) {
+      this.list = list.list
+      this.gf = list.gf
+      this.infinite = list.infinite
+    } else { // generator function
+      this.gf = list
+      this.infinite = infinite
     }
   }
   head() {
@@ -44,86 +65,203 @@ class List {
     return this.slice(1)
   }
   init() {
+    if (xs.infinite) return new List(xs)
+
     return this.slice(0, -1)
   }
   last() {
+    if (this.infinite) throw new Error('no `last` for infinite list')
+
     return this.slice(-1).get(0)
   }
   length() {
+    if (this.infinite) throw new Error('no `length` for infinite list')
+
+    if (this.gf) {
+      const g = this.createGenerator()
+
+      let i = 0
+      while (!g.next().done) i++
+      return i
+    }
     return this.list.length
   }
   toList() {
-    if (this.list) {
-      return this.list
-    } else {
+    if (this.infinite) throw new Error('no `toList` for infinite list')
+
+    if (this.gf) {
+      const g = this.createGenerator()
       const list = []
       while (1) {
-        const o = this.g.next()
-        if (o.done) break
-        list[list.length] = o.value
+        const obj = g.next()
+        if (obj.done) break
+        list[list.length] = obj.value
       }
       return list
     }
+    return this.list
   }
   get(i) {
+    if (i < 0) return undefined
+
+    if (this.gf) {
+      const g = this.createGenerator()
+      let obj = g.next()
+      while (i--) {
+        if (obj.done) return undefined
+        obj = g.next()
+      }
+      return obj.value
+    }
     return this.list[i]
   }
   nil() {
     return !this.length()
   }
+
   take(n) {
-    // FIXME: keep position or not?
+    if (n < 0) return List.empty
+    // keep position
     if (this.list) {
-      return this.slice(0, n)
+      return new List(this.list.splice(0, n))
     }
 
-    const g = this.g
+    const g = this.createGenerator()
     return new List(function* () {
+      let obj = g.next()
       while (n--) {
-        const o = g.next()
-        if (o.done) {
+        if (obj.done) {
           yield undefined
         } else {
-          yield o.value
+          yield obj.value
+          obj = g.next()
         }
       }
-    })
+    }, false)
   }
   drop(n) {
     return this.slice(n)
   }
   cons(x) {
-    this.list.splice(0, 0, x)
-    return this
+    if (this.gf) {
+      const g = this.createGenerator()
+      return new List(function *() {
+        yield x
+        yield* g
+      }, this.infinite)
+    }
+    return new List([x].concat(this.list))
   }
   append(xs) {
-    this.list = this.list.concat(xs.toList())
-    return this
+    if (this.infinite) return new List(this)
+
+    if (this.list && xs.list) {
+      return new List(this.list.concat(xs.toList()))
+    } else {
+      const g = this.createGenerator()
+      return new List(function *() {
+        yield* this.createGenerator()
+        yield* xs.createGenerator()
+      }, xs.infinite)
+    }
   }
   slice(i, j) {
+    if (this.gf) {
+      if (typeof i === 'undefined') i = 0
+      if (typeof j === 'undefined') j = Infinity
+      if (i < 0 || j < 0) {
+        const length = this.length()
+        if (i < 0) i += length
+        if (j < 0) j += length
+      }
+
+      const g = this.createGenerator()
+      const list = []
+      let index = 0
+      while (index < j) {
+        const obj = g.next()
+        if (obj.done) break
+        if (index >= i) list[list.length] = obj.value
+        index++
+      }
+      return new List(list)
+    }
     return new List(this.list.slice(i, j))
   }
+
   map(fn) {
+    if (this.gf) {
+      const g = this.createGenerator()
+      return new List(function* () {
+        while (1) {
+          const obj = g.next()
+          if (obj.done) break
+          yield fn(obj.value)
+        }
+      }, this.infinite)
+    }
     return new List(this.list.map(fn))
   }
   filter(fn) {
+    if (this.gf) {
+      const g = this.createGenerator()
+      return new List(function* () {
+        while (1) {
+          const obj = g.next()
+          if (obj.done) break
+          if (fn(obj.value)) yield obj.value
+        }
+      }, this.infinite)
+    }
     return new List(this.list.filter(fn))
   }
   reverse() {
-    const list = this.list.map((x, i) => this.list[this.list.length - 1 - i])
-    return new List(list)
+    if (this.infinite) throw new Error('no `reverse` for infinite list')
+
+    const list = this.toList()
+    const reversed = list.map((x, i) => list[list.length - 1 - i])
+    return new List(reversed)
   }
   concat() {
-    return this.list.reduce((lst, item) => lst.append(item), List.empty)
+    const g = this.createGenerator()
+    let ret = List.empty
+    while (1) {
+      const obj = g.next()
+      if (obj.done) break
+      ret = ret.append(obj.value)
+      if (obj.value.infinite) ret.infinite = true
+    }
+    return ret
   }
   concatMap(fn) {
-    return this.list.reduce((lst, item) => lst.append(fn(item)), List.empty)
+    const g = this.createGenerator()
+    let ret = List.empty
+    while (1) {
+      const obj = g.next()
+      if (obj.done) break
+      ret = ret.append(fn(obj.value))
+      if (obj.value.infinite) ret.infinite = true
+    }
+    return ret
   }
   zipWith(fn, xs) {
-    const list = this.list.map((x, i) => fn(x, xs.get(i)))
-    return new List(list)
+    const g = this.createGenerator()
+    const g2 = xs.createGenerator()
+    return new List(function* () {
+      while (1) {
+        const obj = g.next()
+        const obj2 = g2.next()
+        if (obj.done && obj2.done) break
+        yield fn(obj.value, obj2.value)
+      }
+    }, this.infinite || xs.infinite)
   }
   foldr(fn, x) {
+    if (this.gf) {
+      // TODO: how can we know `fn` is nullary or unary?
+      throw new Error('not implemented')
+    }
+
     let r = x
     for (let i = this.list.length - 1; i >= 0; i--) {
       r = fn(this.list[i], r)
@@ -131,6 +269,18 @@ class List {
     return r
   }
   foldl(fn, x) {
+    if (this.infinite) throw new Error('no `foldl` for infinite list')
+
+    if (this.gf) {
+      const g = this.createGenerator()
+      let ret = x
+      while (1) {
+        const obj = g.next()
+        if (obj.done) break
+        ret = fn(ret, obj.value)
+      }
+      return ret
+    }
     return this.list.reduce((s, item) => fn(s, item), x)
   }
   scanr(fn, x) {
@@ -142,43 +292,87 @@ class List {
     return new List(r)
   }
   scanl(fn, x) {
-    let r = []
-    r[0] = x
-    for (let i = 0; i < this.list.length; i++) {
-      r[i + 1] = fn(r[i], this.list[i])
+    const r = [x]
+    const g = this.createGenerator()
+
+    let prev = x
+    while (1) {
+      const obj = g.next()
+      if (obj.done) break
+      prev = fn(prev, obj.value)
+      r.push(prev)
     }
     return new List(r)
   }
   elem(x) {
-    return this.list.includes(x)
+    if (this.infinite) throw new Error('no `elem` for infinite list')
+
+    return this.any(item => item === x)
   }
   elemIndex(x) {
-    return this.list.indexOf(x)
+    if (this.infinite) throw new Error('no `elemIndex` for infinite list')
+
+    const g = this.createGenerator()
+    let i = 0
+    while (1) {
+      const obj = g.next()
+      if (obj.done) break
+      if (x === obj.value) return i
+      i++
+    }
+    return -1
   }
   find(fn) {
-    return this.get(this.findIndex(fn))
+    if (this.infinite) throw new Error('no `find` for infinite list')
+
+    const g = this.createGenerator()
+    while (1) {
+      const obj = g.next()
+      if (obj.done) break
+      if (fn(obj.value)) return obj.value
+    }
+    return undefined
   }
   findIndex(fn) {
-    for (let i = 0; i < this.list.length; i++) {
-      if (fn(this.get(i))) {
-        return i
-      }
+    if (this.infinite) throw new Error('no `findIndex` for infinite list')
+
+    const g = this.createGenerator()
+    let i = 0
+    while (1) {
+      const obj = g.next()
+      if (obj.done) break
+      if (fn(obj.value)) return i
+      i++
     }
     return -1
   }
   any(fn) {
-    return this.list.some(fn)
+    if (this.infinite) throw new Error('no `any` for infinite list')
+
+    const g = this.createGenerator()
+    while (1) {
+      const obj = g.next()
+      if (obj.done) break
+      if (fn(obj.value)) return true
+    }
+    return false
   }
   all(fn) {
-    return this.list.every(fn)
+    if (this.infinite) throw new Error('no `all` for infinite list')
+
+    const g = this.createGenerator()
+    while (1) {
+      const obj = g.next()
+      if (obj.done) break
+      if (!fn(obj.value)) return false
+    }
+    return true
   }
   the() {
-    if (this.length()) {
-      const val = this.get(0)
-      const same = this.all(x => x === val)
-      if (same) return this.head()
-    }
-    return undefined
+    if (this.infinite) throw new Error('no `the` for infinite list')
+
+    const x = this.head()
+    return this.all(item => item === x) ? x : undefined
   }
 }
 
